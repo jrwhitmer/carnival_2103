32456e45413f78f12a878ea522db316ae9512c4a
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-15 11:29:53 -0400

    Create test for attr_reader of name in carnvial class

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -11,5 +11,10 @@ RSpec.describe Carnival do
 
       expect(jeffco_fair).to be_instance_of(Carnival)
     end
+    it 'can return name' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+
+      expect(jeffco_fair.name).to eq("Jefferson County Fair")
+    end
   end
 end
