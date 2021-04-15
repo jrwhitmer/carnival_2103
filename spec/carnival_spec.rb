9cb9a5f84d781c2725033097ba0020d6e2543f82
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-15 11:36:31 -0400

    Create additional test for add_ride  method

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -30,5 +30,16 @@ RSpec.describe Carnival do
 
       expect(jeffco_fair.rides).to eq([ferris_wheel])
     end
-  end 
+    it 'can store multiple rides' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+
+      expect(jeffco_fair.rides).to eq([ferris_wheel, bumper_cars, scrambler])
+    end
+  end
 end
