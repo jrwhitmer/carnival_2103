7aa2f92acb080a31a6c03cef37ecbae4cf3c1abe
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-15 11:54:05 -0400

    Create test for attendees attr_reader in carnival class

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -21,6 +21,11 @@ RSpec.describe Carnival do
 
       expect(jeffco_fair.rides).to eq([])
     end
+    it 'has no attendees by default' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+
+      expect(jeffco_fair.attendees).to eq([])
+    end
   end
   context '#add_ride' do
     it 'can add a ride' do
@@ -64,6 +69,26 @@ RSpec.describe Carnival do
 
       expect(jeffco_fair.recommend_rides(bob)).to eq([ferris_wheel, bumper_cars])
       expect(jeffco_fair.recommend_rides(sally)).to eq([scrambler])
+    end
+  end
+
+  context '#attendees_by_ride_interest' do
+    xit 'can return a hash of attendees interested in each ride' do
+      jeffco_fair = Carnival.new("Jefferson County Fair")
+
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+
+      bob = Attendee.new('Bob', 20)
+      bob.add_interest('Ferris Wheel')
+      bob.add_interest('Bumper Cars')
+
+      sally = Attendee.new('Sally', 20)
     end
   end
 end
